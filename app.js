2bc5321e8f6bb766d2e99e1cfaf92bea83461707
pouryawJs <pourya.bu@gmail.com>
2024-08-26 12:32:49 +0430

    send Home menu

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const { Telegraf } = require("telegraf");
+const { Telegraf, Markup } = require("telegraf");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const token = process.env.BOT_TOKEN;
 
 const bot = new Telegraf(token);
+
+bot.start((ctx) => {
+    ctx.reply(
+        "Choose your GPT version 🤖",
+        Markup.inlineKeyboard([
+            [
+                Markup.button.callback("ChatGPT 3.5", "gpt3.5-turbo"),
+                Markup.button.callback("ChatGPT4-o", "gpt4o"),
+            ],
+            [Markup.button.callback("ChatGPT 4-turbo", "gpt4-turbo")],
+        ])
+    );
+});
 
 bot.launch(() => {
     console.log("bot started successfully");
